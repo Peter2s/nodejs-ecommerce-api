f66bc55c4e4de9f98cda92f46f49657143cab541
peter salah <petersalahsamir@gmail.com>
2023-05-29 22:48:47 +0300

    remove item from cart , get cart , clear cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,13 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-const { addProductToCart } = require("../services/cartSerivce");
+const {
+  addProductToCart,
+  getCart,
+  removeProductFromCart,
+  clearCart,
+} = require("../services/cartSerivce");
 
 const { authenticate, authorizedTo } = require("../services/authSerivce");
 
 router.use(authenticate, authorizedTo("user"));
-router.route("/").get().post(addProductToCart);
+router.route("/").get(getCart).post(addProductToCart).patch().delete(clearCart);
 
-router.route("/:id").get().patch().delete();
+router.route("/:id").delete(removeProductFromCart);
 
 module.exports = router;
--- services/cartSerivce.js
+++ services/cartSerivce.js
@@ -1,10 +1,38 @@
 const AsyncHandler = require("express-async-handler");
 const ProductModel = require("../models/productModel");
 const CartModel = require("../models/cartModel");
+const ApiError = require("../utils/ApiError");
+
+const CalclateCartTotalPrice = (cart) => {
+  let cartTotalPrice = 0;
+
+  cart.cartItems.forEach(
+    // eslint-disable-next-line no-return-assign
+    (item) => (cartTotalPrice += item.price * item.quantity)
+  );
+
+  return cartTotalPrice;
+};
+/*
+ * @description get cart for authenticated  user
+ * @route GET /api/v1/cart
+ * @access private
+ *
+ */
+module.exports.getCart = AsyncHandler(async (req, res, next) => {
+  const cart = await CartModel.findOne({
+    user: req.user._id,
+  });
+  if (!cart) next(new ApiError("there is not cart ", 404));
+
+  res
+    .status(200)
+    .json({ status: "success", cartItems: cart.cartItems.length, data: cart });
+});
 
 /*
  * @description add product to cart
- * @route POST /api/v1/categories
+ * @route POST /api/v1/cart
  * @access private
  *
  */
@@ -41,5 +69,45 @@ module.exports.addProductToCart = AsyncHandler(async (req, res) => {
       });
     }
   }
+  cart.totalPrice = CalclateCartTotalPrice(cart);
   await cart.save();
+});
+/*
+ * @description remove product from cart
+ * @route DELETE /api/v1/cart
+ * @access private[user]
+ */
+module.exports.removeProductFromCart = AsyncHandler(async (req, res, next) => {
+  const { itemId } = req.body;
+  const cart = await CartModel.findOneAndUpdate(
+    { user: req.user._id },
+    {
+      $pull: {
+        cartItems: {
+          _id: itemId,
+        },
+      },
+    },
+    { new: true }
+  );
+  cart.totalPrice = CalclateCartTotalPrice(cart);
+  cart.save();
+  res.status(200).json({
+    status: "success",
+    message: "product successfully removed",
+    data: cart,
+  });
+});
+
+/*
+ * @description clear cart for authenticated user
+ * @route DELETE /api/v1/cart
+ * @access private[user]
+ */
+
+module.exports.clearCart = AsyncHandler(async (req, res) => {
+  await CartModel.findOneAndDelete({ user: req.user._id });
+  res
+    .status(200)
+    .json({ status: "success", message: "cart cleared successfully" });
 });
